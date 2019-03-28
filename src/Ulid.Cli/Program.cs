b0635ab61da909cfc59939e4ee3ade0615ef5498
Yusuke Ito <itn3000@gmail.com>
2019-03-28 14:58:01 +0900

    add Ulid.Cli project.

---
+++ src/Ulid.Cli/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MicroBatchFramework;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+
+namespace Ulid.Cli
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await new HostBuilder().RunBatchEngineAsync<UlidBatch>(args).ConfigureAwait(false);
+        }
+    }
+    public class UlidBatch : BatchBase
+    {
+        [Command("new", "create new ULID")]
+        public void New([Option("b", "output as base64 format")]bool base64 = false)
+        {
+            var ulid = System.Ulid.NewUlid(DateTimeOffset.Now);
+            Console.Write(base64 ? ulid.ToBase64() : ulid.ToString());
+        }
+    }
+}
