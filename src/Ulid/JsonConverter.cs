3a30753e7529972cce8e461963ce67a27dedc32b
Rynco Maekawa <lynzrand@outlook.com>
2020-02-11 11:48:05 +0800

    Fix reader method to catch and rethrow JSON exceptions

--- src/Ulid/JsonConverter.cs
+++ src/Ulid/JsonConverter.cs
@@ -15,7 +15,18 @@ namespace System._Ulid
         /// <returns>The parsed value</returns>
         public override Ulid Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
-            return Ulid.Parse(reader.GetString());
+            try
+            {
+                return Ulid.Parse(reader.GetString());
+            }
+            catch (ArgumentException e)
+            {
+                throw new JsonException("Ulid invalid: length must be 26", e);
+            }
+            catch (OverflowException e)
+            {
+                throw new JsonException("Ulid invalid: invalid character", e);
+            }
         }
 
         public override void Write(Utf8JsonWriter writer, Ulid value, JsonSerializerOptions options)
