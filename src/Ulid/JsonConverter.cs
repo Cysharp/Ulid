6af28597ba954bfe07f24229c07c8889d0b56b54
Rynco Maekawa <lynzrand@outlook.com>
2020-02-11 16:35:30 +0800

    Format files

--- src/Ulid/JsonConverter.cs
+++ src/Ulid/JsonConverter.cs
@@ -4,41 +4,43 @@ using System.Text.Json.Serialization;
 
 namespace System
 {
-public partial struct Ulid{
-    public class UlidJsonConverter : JsonConverter<Ulid>
+    public partial struct Ulid
     {
-        /// <summary>
-        /// Read a Ulid value represented by a string from JSON.
-        /// </summary>
-        /// <param name="reader"></param>
-        /// <param name="typeToConvert"></param>
-        /// <param name="options"></param>
-        /// <returns>The parsed value</returns>
-        public override Ulid Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        public class UlidJsonConverter : JsonConverter<Ulid>
         {
-            try
+            /// <summary>
+            /// Read a Ulid value represented by a string from JSON.
+            /// </summary>
+            /// <param name="reader"></param>
+            /// <param name="typeToConvert"></param>
+            /// <param name="options"></param>
+            /// <returns>The parsed value</returns>
+            public override Ulid Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
             {
-                return Ulid.Parse(reader.GetString());
+                try
+                {
+                    return Ulid.Parse(reader.GetString());
+                }
+                catch (ArgumentException e)
+                {
+                    throw new JsonException("Ulid invalid: length must be 26", e);
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    throw new JsonException("Ulid invalid: length must be 26", e);
+                }
+                catch (OverflowException e)
+                {
+                    throw new JsonException("Ulid invalid: invalid character", e);
+                }
             }
-            catch (ArgumentException e)
-            {
-                throw new JsonException("Ulid invalid: length must be 26", e);
-            }
-            catch (IndexOutOfRangeException e)
-            {
-                throw new JsonException("Ulid invalid: length must be 26", e);
-            }
-            catch (OverflowException e)
-            {
-                throw new JsonException("Ulid invalid: invalid character", e);
-            }
-        }
 
-        public override void Write(Utf8JsonWriter writer, Ulid value, JsonSerializerOptions options)
-        {
-            var toString = value.ToString();
-            writer.WriteStringValue(toString);
+            public override void Write(Utf8JsonWriter writer, Ulid value, JsonSerializerOptions options)
+            {
+                var toString = value.ToString();
+                writer.WriteStringValue(toString);
+            }
         }
     }
-}}
+}
 #endif
